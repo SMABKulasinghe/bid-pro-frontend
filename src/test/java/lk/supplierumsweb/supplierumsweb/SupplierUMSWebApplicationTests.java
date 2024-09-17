fc8e79571744528c53ea03180bec02bad0594cec
SMABKulasinghe <74065043+SMABKulasinghe@users.noreply.github.com>
2024-09-17 22:49:34 +0530

    Test class

---
+++ src/test/java/lk/supplierumsweb/supplierumsweb/SupplierUMSWebApplicationTests.java
@@ -0,0 +1,16 @@
+package lk.supplierumsweb.supplierumsweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupplierUMSWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
